cb377b42b99233c8e3d5671664ad224a19fab835
Anderson Cruz <andersonc859828@gmail.com>
2025-02-12 16:12:42 -0300

    style

---
+++ script.js
@@ -0,0 +1,20 @@
+let list = document.querySelectorAll('.item');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let count = list.length;
+let active = 0;
+
+next.onclick = () => {
+    list[active].classList.remove('active');
+    active = (active + 1) % count;
+    list[active].classList.add('active');
+
+}
+
+prev.onclick = () => {
+    list[active].classList.remove('active');
+    active = (active - 1 + count) % count;
+    list[active].classList.add('active');
+
+}
